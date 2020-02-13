53c7b88455e8b43c3990049ee4d5c92075c37f17
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-13 14:16:32 -0700

    Update the pages update

--- RazorPagesMovie/Pages/Movies/Edit.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Edit.cshtml.cs
@@ -9,69 +9,67 @@ using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
 
-namespace RazorPagesMovie
+namespace RazorPagesMovie.Pages.Movies
 {
-    public class EditModel : PageModel
+  public class EditModel : PageModel
+  {
+    private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+    public EditModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
-
-        public EditModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
-
-        [BindProperty]
-        public Movie Movie { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
-
-            if (Movie == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Attach(Movie).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieExists(Movie.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool MovieExists(int id)
-        {
-            return _context.Movie.Any(e => e.ID == id);
-        }
+      _context = context;
     }
+
+    [BindProperty]
+    public Movie Movie { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+      if (Movie == null)
+      {
+        return NotFound();
+      }
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      _context.Attach(Movie).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!MovieExists(Movie.ID))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return RedirectToPage("./Index");
+    }
+
+    private bool MovieExists(int id)
+    {
+      return _context.Movie.Any(e => e.ID == id);
+    }
+  }
 }
