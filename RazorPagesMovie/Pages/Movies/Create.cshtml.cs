ca07fc4192197f5b12628b386b454fc1b0089d73
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-13 13:17:59 -0700

    Scaffolding update

--- RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -27,19 +27,19 @@ namespace RazorPagesMovie
         [BindProperty]
         public Movie Movie { get; set; }
 
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // more details see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
 
-            _context.Movie.Add(Movie);
-            await _context.SaveChangesAsync();
+      _context.Movie.Add(Movie);
+      await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
-        }
+      return RedirectToPage("./Index");
+    }
     }
 }
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -8,22 +8,22 @@ using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
 
-namespace RazorPagesMovie
+namespace RazorPagesMovie.Pages.Movies
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+    public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
-
-        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Movie> Movie { get;set; }
-
-        public async Task OnGetAsync()
-        {
-            Movie = await _context.Movie.ToListAsync();
-        }
+      _context = context;
     }
+
+    public IList<Movie> Movie { get; set; }
+
+    public async Task OnGetAsync()
+    {
+      Movie = await _context.Movie.ToListAsync();
+    }
+  }
 }
