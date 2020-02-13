5fec84a88cf455fffef8ff43a7d1acad10727bf1
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-13 13:26:45 -0700

    Scaffolding update

--- RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -1,34 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// Unused usings removed.
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
+using System;
+using System.Threading.Tasks;
 
-namespace RazorPagesMovie
+namespace RazorPagesMovie.Pages.Movies
 {
-    public class CreateModel : PageModel
+  public class CreateModel : PageModel
+  {
+    private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+    public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+      _context = context;
+    }
 
-        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
+    public IActionResult OnGet()
+    {
+      return Page();
+    }
 
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+    [BindProperty]
+    public Movie Movie { get; set; }
 
-        [BindProperty]
-        public Movie Movie { get; set; }
-
-    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-    // more details see https://aka.ms/RazorPagesCRUD.
     public async Task<IActionResult> OnPostAsync()
     {
       if (!ModelState.IsValid)
@@ -41,5 +37,5 @@ namespace RazorPagesMovie
 
       return RedirectToPage("./Index");
     }
-    }
+  }
 }
