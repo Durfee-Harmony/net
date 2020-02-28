e7fc4b18f3fddb6735e875976b36a95ab3316e70
ryanlukecross <cro16052@byui.edu>
2020-02-28 16:01:39 -0700

    deskquote.create.cshtml

--- MegaDesk/Pages/DeskQuotes/Create.cshtml.cs
+++ MegaDesk/Pages/DeskQuotes/Create.cshtml.cs
@@ -14,10 +14,13 @@ namespace MegaDesk.Pages.DeskQuotes
    {
       private readonly MegaDesk.Data.MegaDeskContext _context;
 
-      int Width { get; set; }
-      int Depth { get; set; }
-      int NumberOfDrawers { get; set; }
-      int DesktopMaterialID { get; set; }
+      int DeskWidth { get; set; }
+      int DeskDepth { get; set; }
+      int DeskNumberOfDrawers { get; set; }
+      int DeskDesktopMaterialID { get; set; }
+      int BASE_PRICE = 200;
+      decimal FinalPrice;
+      int DaysToComplete;
 
 
       public CreateModel(MegaDesk.Data.MegaDeskContext context)
@@ -43,19 +46,103 @@ namespace MegaDesk.Pages.DeskQuotes
             return Page();
          }
 
-         this.Width = Width;
-         this.Depth = Depth;
-         this.NumberOfDrawers = NumberOfDrawers;
-         this.DesktopMaterialID = DesktopMaterialID;
+         this.DeskWidth = DeskWidth;
+         this.DeskDepth = DeskDepth;
+         this.DeskNumberOfDrawers = DeskNumberOfDrawers;
+         this.DeskDesktopMaterialID = DeskDesktopMaterialID;
 
+
+
+
+         Desk desk = new Desk(DeskWidth, DeskDepth, DeskNumberOfDrawers, DeskDesktopMaterialID);
+         DeskQuote deskQuote = new DeskQuote();
+
+
+         decimal deskPrice = (decimal)this.BASE_PRICE;
+         decimal surfaceArea = desk.Width * desk.Depth;
+         decimal numberOfDrawers = desk.NumberOfDrawers;
+         DesktopMaterial material = desk.DesktopMaterial;
+         int[] PRICES = { 60, 70, 80, 40, 50, 60, 30, 35, 40 };
+
+         deskPrice += numberOfDrawers * 50M;
+
+         if (surfaceArea > 1000M)
+         {
+            deskPrice += surfaceArea - 1000M;
+         }
+
+         if (material.Equals(1))
+         {
+            deskPrice += 200M;
+         }
+         else if (material.Equals(2))
+         {
+            deskPrice += 100M;
+         }
+         else if (material.Equals(3))
+         {
+            deskPrice += 50M;
+         }
+         else if (material.Equals(4))
+         {
+            deskPrice += 300M;
+         }
+         else if (material.Equals(5))
+         {
+            deskPrice += 125M;
+         }
+
+         if (DaysToComplete == 3)
+         {
+            if (surfaceArea < 1000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[0]);
+            }
+            else if (surfaceArea <= 2000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[1]);
+            }
+            else
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[2]);
+            }
+         }
+         else if (DaysToComplete == 5)
+         {
+            if (surfaceArea < 1000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[3]);
+            }
+            else if (surfaceArea <= 2000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[4]);
+            }
+            else
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[5]);
+            }
+         }
+         else if (DaysToComplete == 7)
+         {
+            if (surfaceArea < 1000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[6]);
+            }
+            else if (surfaceArea <= 2000)
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[7]);
+            }
+            else
+            {
+               deskPrice += System.Convert.ToDecimal(PRICES[8]);
+            }
+         }
+         deskQuote.FinalPrice = deskPrice;
 
 
          // DO LOGIC
-         Desk desk = new Desk(Width, Depth, NumberOfDrawers, DesktopMaterialID);
          _context.Desk.Add(desk);
-         DeskQuote deskQuote = new DeskQuote();
          _context.DeskQuote.Add(deskQuote);
-
 
 
          // FINISH LOGIC
