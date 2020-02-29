059e3be69cb65503e722464329091759b4977f54
marencamille19 <ste18009@byui.edu>
2020-02-29 16:18:14 -0700

    Changed id name

--- MegaDesk/Models/DaysToComplete.cs
+++ MegaDesk/Models/DaysToComplete.cs
@@ -6,7 +6,7 @@ namespace MegaDesk.Models
 {
    public class DaysToComplete
    {
-       public int DaysToCompleteID { get; set; }
+       public int ID { get; set; }
        public string Description { get; set; }
        public int Days { get; set; }
    }
