942fe4faf2eaf35effb70214474dd52ba8b720fd
Durfee-Harmony <harmonydurfee@gmail.com>
2020-04-10 14:49:12 -0700

    update

---
+++ Final/Final/Models/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+  public class Speaker
+  {
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstMidName { get; set; }
+    public DateTime SpeakingDate { get; set; }
+
+    public ICollection<Meeting> Meetings { get; set; }
+  }
+}
