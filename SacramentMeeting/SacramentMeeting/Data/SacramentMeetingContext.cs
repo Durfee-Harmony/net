672ea694c75d29639b4bb11f522ea05e7ada13d3
Durfee-Harmony <harmonydurfee@gmail.com>
2020-07-13 12:51:57 -0700

    update

---
+++ SacramentMeeting/SacramentMeeting/Data/SacramentMeetingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SacramentMeeting.Models;
+
+namespace SacramentMeeting.Data
+{
+    public class SacramentMeetingContext : DbContext
+    {
+        public SacramentMeetingContext(DbContextOptions<SacramentMeetingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Meeting> Meeting { get; set; }
+    }
+}
